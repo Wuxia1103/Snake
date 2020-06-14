480b6d1fe66fe456c9cee24885c5ce83b2be04ab
吴霞 <2633659610@qq.com>
2020-06-14 21:43:53 +0800

    ...

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -29,45 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.btExit = new System.Windows.Forms.Button();
-            this.btBegin = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btExit
-            // 
-            this.btExit.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btExit.Location = new System.Drawing.Point(637, 370);
-            this.btExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btExit.Name = "btExit";
-            this.btExit.Size = new System.Drawing.Size(111, 38);
-            this.btExit.TabIndex = 0;
-            this.btExit.Text = "退出游戏";
-            this.btExit.UseVisualStyleBackColor = true;
-            this.btExit.Click += new System.EventHandler(this.btExit_Click);
-            // 
-            // btBegin
-            // 
-            this.btBegin.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btBegin.Location = new System.Drawing.Point(130, 370);
-            this.btBegin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btBegin.Name = "btBegin";
-            this.btBegin.Size = new System.Drawing.Size(111, 38);
-            this.btBegin.TabIndex = 1;
-            this.btBegin.Text = "开始游戏";
-            this.btBegin.UseVisualStyleBackColor = true;
-            this.btBegin.Click += new System.EventHandler(this.btBegin_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(47, 17);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(642, 347);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
