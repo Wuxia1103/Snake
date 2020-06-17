f65bd921003f73976ea8917e27371b1ecffaf1a9
吴霞 <2633659610@qq.com>
2020-06-17 18:29:27 +0800

    改成了列表方式

--- Snake/Snake/Form2.Designer.cs
+++ Snake/Snake/Form2.Designer.cs
@@ -35,6 +35,7 @@
             // timer1
             // 
             this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
             // 
             // Form2
             // 
--- Snake/Snake/Form2.cs
+++ Snake/Snake/Form2.cs
@@ -12,158 +12,122 @@ namespace Snake
 {
     public partial class Form2 : Form
     {
-        string key = "start";//记录键盘状态
-        Label[] labels = new Label[3000];//贪吃蛇身体数组
-        Timer dt = new Timer();
-        Random R = new Random();//随机数,用于生成食物
-        int a = 0, b = 0;// 记录坐标
+        string key = "D";//记录键盘状态
+        List<Label> labels = new List<Label>();//贪吃蛇身体数组
+        Random R = new Random();//随机数,用于生成食物坐标
+        int snakelen = 5;
         public Form2()
         {
             InitializeComponent();
         }
         private void Form2_Load(object sender, EventArgs e)
         {
-                this.Top = 120;
-                this.Left = 120;
-                this.Width = 1000;
-                this.Height = 500;
-                this.BackColor = Color.White;
-                //贪吃蛇设置
-                for (int i = 0; i < 5; i++)
-                {
-                    Label label = new Label();
-                    label.Width = label.Height = 10;
-                    label.Top = 400;
-                    label.Left = 400;
-                    label.BackColor = Color.Black;
-                    //获取或设置包含有关控件的数据的对象。
-                    label.Tag = i;
-                    labels[i] = label;
-                    this.Controls.Add(label);
-                }
-                //控件timer,每隔一段时间发生一次右移
-                dt.Tick += new EventHandler(dt_Tick);
-                //键盘敲击事件
-                this.KeyDown += new KeyEventHandler(form_keyDown);
-                dt.Start();//timer开始计时
+            this.Top = 120;
+            this.Left = 120;
+            this.Width = 1000;
+            this.Height = 500;
+            this.BackColor = Color.White;
+            //贪吃蛇设置
+            int x = 10, y = 10;
+            for (int i = 0; i < snakelen;)
+            {
+                Label label = new Label();
+                label.BackColor = Color.Black;
+                label.ForeColor = Color.Red;
+                label.AutoSize = false;
+                label.Size = new Size(10, 10);
+                label.Location = new Point(x * 10, y * 10);
+                x--;
+                this.Controls.Add(label);
+                labels.Add(label);
+                i++;
+            }
             display();
         }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Snake_move();
+            eat_food();
+            timer1.Enabled = true;
+        }
+        /// <summary>
+        /// 键盘响应事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string a = Convert.ToString(e.KeyCode);
+            switch (a)
+            {
+                case "W": key = "W"; break;//上
+                case "A": key = "A"; break;//左
+                case "S": key = "S"; break;//下
+                case "D": key = "D"; break;//右
+                default: break;
+            }
+        }
         //蛇移动
-        void Snake_move(int m, int n)
+        void Snake_move()
         {
-            int x = 0, y = 0;
-            for (int i = 1; labels[i] != null; i++)
+            switch (key)
             {
-                if (i >= 3)
-                {
-                    x = a;
-                    y = b;
-                }
-                if (i == 1)
-                {
-                    x = labels[i].Left;
-                    y = labels[i].Top;
-                    labels[i].Left = m;
-                    labels[i].Top = n;
-                }
-                else
-                {
-                    a = labels[i].Left;
-                    b = labels[i].Top;
-                    labels[i].Left = x;
-                    labels[i].Top = y;
-                }
+                case "D":
+                    for (int i = snakelen - 1; i > 0;)
+                    {
+                        labels[i].Left = labels[i - 1].Left;
+                        labels[i].Top = labels[i - 1].Top;
+                        labels[i - 1].Left += 10;
+                        i--;
+                    }
+                    break;
+                case "A":
+                    for (int i = snakelen - 1; i > 0;)
+                    {
+                        labels[i].Left = labels[i - 1].Left;
+                        labels[i].Top = labels[i - 1].Top;
+                        labels[i - 1].Left -= 10;
+                        i--;
+                    }
+                    break;
+                case "S":
+                    for (int i = snakelen - 1; i > 0;)
+                    {
+                        labels[i].Left = labels[i - 1].Left;
+                        labels[i].Top = labels[i - 1].Top;
+                        labels[i - 1].Top += 10;
+                        i--;
+                    }
+                    break;
+                case "W":
+                    for (int i = snakelen - 1; i > 0;)
+                    {
+                        labels[i].Left = labels[i - 1].Left;
+                        labels[i].Top = labels[i - 1].Top;
+                        labels[i - 1].Top -= 10;
+                        i--;
+                    }
+                    break;
             }
         }
-        /// <summary>
-        /// 敲击键盘响应
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        void form_keyDown(object sender, KeyEventArgs e)
-        {
-            int m, n;
-            m = labels[0].Left;
-            n = labels[0].Top;
-            //获取按了什么键
-            key = e.KeyCode.ToString();
-            //右
-            if (e.KeyCode.ToString() == "D")
-            {
-                labels[0].Left = m + 10;
-            }
-            //左
-            if (e.KeyCode.ToString() == "A")
-            {
-                labels[0].Left = m - 10;
-            }
-            //上
-            if (e.KeyCode.ToString() == "W")
-            {
-                labels[0].Top = n - 10;
-            }
-            //下
-            if (e.KeyCode.ToString() == "S")
-            {
-                labels[0].Top = n + 10;
-            }
-            eat_food();
-        }
-        /// <summary>
-        /// 键盘按键输入的响应
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        void dt_Tick(object sender, EventArgs e)
-        {
-            int m, n;
-            m = labels[0].Left;
-            n = labels[0].Top;
-            if (key == "start")
-            {
-                labels[0].Left = m + 10;
-                Snake_move(m, n);
-            }
-            //右
-            if (key == "D")
-            {
-                labels[0].Left = m + 10;
-                Snake_move(m, n);
-            }
-            //左
-            if (key == "A")
-            {
-                labels[0].Left = m - 10;
-                Snake_move(m, n);
-            }
-            //上
-            if (key == "W")
-            {
-                labels[0].Top = n - 10;
-                Snake_move(m, n);
-            }
-            //下
-            if (key == "S")
-            {
-                labels[0].Top = n + 10;
-                Snake_move(m, n);
-            }
-            //每按一次，判断是否与食物重合
-            eat_food();
-        }
+        
         //吃过食物，蛇的身体变长
         void Snake_eat()
         {
-            int m, n;
-            m = labels[0].Left;
-            n = labels[0].Top;
             Label lb = new Label();
             lb.BackColor = Color.Black;
             lb.AutoSize = false;
             lb.Size = new Size(10, 10);
-            lb.Location = labels[0].Location;
-            Snake_move(m, n);
+            lb.Location =labels[snakelen-1].Location;
             this.Controls.Add(lb);
+            labels.Add(lb);
+            snakelen++;
         }
         /// <summary>
         /// 创建食物
@@ -175,12 +139,16 @@ namespace Snake
             y = R.Next(36);
             Label lb = new Label();
             lb.BackColor = Color.Red;
-            lb.AutoSize = false;
+            lb.AutoSize = false;//如果不关闭的话不能够设置size属性
             lb.Size = new Size(10, 10);
             lb.Text = "";
             lb.Location = new Point(x * 10, y * 10);
             this.Controls.Add(lb);
+            
         }
+        /// <summary>
+        /// 吃食物
+        /// </summary>
         public void eat_food()
         {
             foreach (Label lb in this.Controls)
@@ -196,6 +164,6 @@ namespace Snake
                 }
             }
         }
-        
+       
     }
 }
