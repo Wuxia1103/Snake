0452c30ca8b092d66dd5e08e153c03c62bcc4c4d
chanyeoll1127 <1194396266@qq.com>
2020-06-17 21:15:55 +0800

    修改。。。

--- Snake/Snake/Food.cs
+++ Snake/Snake/Food.cs
@@ -9,51 +9,50 @@ namespace Snake
 {
     class Food
     {
-        //位置
-        private Point _origin;
-        private static int unit = 1;
-        //场地长
-        private int length = unit * 80;
-        //场地宽
-        private int width = unit *60;
-
-        
-        public Point Origin{
-            get{return _origin;}
-            set{_origin = value;}
-        }
-
-
-        //显示食物
-        public void Display(Graphics g)
+        List<Label> labels = new  List<Label>();
+        Random food = new Random();
+        int snakelen = 5;
+         // 利用随机函数生成食物
+         void display()
         {
-            SolidBrush brush = new SolidBrush(Color.Red);
-            g.FillRectangle(brush,_origin.X,_origin.Y,1,1);
+            int x, y;//表示食物点的坐标
+            x = food.Next(55);
+            y = food.Next(66);
+            Label lb = new Label();
+            lb.BackColor = Color.Red;
+            lb.AutoSize = false;
+            lb.Size = new Size(10, 10);
+            lb.Text = "hhhh";
+            lb.Location = new Point(x * 10, y * 10);
+            this.Controls.Add(lb);
         }
-        //食物消失
-         public void UnDisplay(Graphics g)
+        //吃到食物，蛇的身体变长
+        void Snake_eat()
         {
-            SolidBrush brush = new SolidBrush(Color.White);  //背景色
-            g.FillRectangle(brush,_origin.X,_origin.Y,1,1);
+            Label lb = new Label();
+            lb.BackColor = Color.Black;
+            lb.AutoSize = false;
+            lb.Size = new Size(10, 10);
+            lb.Location =labels[snakelen-2].Location;
+            this.Controls.Add(lb);
+            labels.Add(lb);
+            snakelen++;
         }
-        //产生随机食物
-        public Food RandomFood()
+        //判断是否吃到食物以及吃到食物后的反应
+        void EatFood()
         {
-            Random random = new Random();
-            int x = random.Next(length/unit-2)+1;
-            int y = random.Next(width/unit-2)+1;
-            Point d = new Point();
-            Food f = new Food();
-            f.Origin = d;
-            return f;
-        }
-        //显示新食物
-        public void DisplayFood(Graphics g)
-        {
-            Food food = new Food();
-            food.UnDisplay(g);
-            food.RandomFood();
-            food.Display(g);
+            foreach (Label a1 in this.Controls)
+            {
+                if (a1.BackColor == Color.Red)
+                {
+                    if (a1.Location == labels[0].Location)
+                    {
+                        this.Controls.Remove(a1);
+                        display();
+                        Snake_eat();
+                    }
+                }
+            }
         }
     }
 }
